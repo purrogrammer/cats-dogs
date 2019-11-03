7bdb0bb8724d696de39439e3cb6cd11c105b9a44
purrogrammer <margueritenyc@hotmail.com>
2019-11-03 14:50:58 -0800

    add matching functionality

--- scripts.js
+++ scripts.js
@@ -4,7 +4,29 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
-}
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true; 
+        firstcard = this;
+    } else {
+        //second click
+        hasFlippedCard = false; 
+        secondCard = this;
+
+        //match logic 
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            //IF match
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            //not a match
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+             }, 2000);
+     }
+    }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
